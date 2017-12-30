866accb2a24754bee76f40bfbf320a5761bad060
Alisa Ono <alisao@mit.edu>
2017-12-30 13:27:14 -0500

    Refactored script.js; Added random generation and regen; Always show trick texts

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const trickToImg = {
   "pike": "./images/generic.jpeg",
 }
 
-const nextTrick = {
+const nextTricks = {
   "allegra": ["superman"],
   "superman": ["shoulder_mount","allegra"],
   "apprentice": ["superman"],
@@ -28,24 +28,50 @@ const nextTrick = {
   "pike": ["jade","gemini"],
 }
 
+const allTricks = Object.keys(nextTricks)
+
 function appendTrick(trick) {
-  let $newdiv = $("<div class='trick'></div>" );
-  $newdiv.append("<img src='"+trickToImg[trick]+"'/>");
-  $newdiv.append("<div class='trick-overlay'></div>");
-  $newdiv.append("<h2 class='trick-text'>"+trick.replace("_"," ").toUpperCase()+"</h2>");
-  $('.window').append($newdiv);
+  let $newdiv = $("<div class='trick'></div>" )
+  $newdiv.append("<img src='"+trickToImg[trick]+"'/>")
+  $newdiv.append("<div class='trick-overlay'></div>")
+  $newdiv.append("<h2 class='trick-text'>"+trick.replace("_"," ").toUpperCase()+"</h2>")
+  $('.window').append($newdiv)
 }
 
 function appendArrow(){
-  $('.window').append("<img class='trick-arrow' src='./images/arrow.png'/>");
+  $('.window').append("<img class='trick-arrow' src='./images/arrow.png'/>")
+}
+
+function renderTricks(trickLists) {
+  $('.window').empty()
+  appendTrick(trickLists[0])
+  for (let i = 1; i < trickLists.length; i++) {
+    appendArrow()
+    appendTrick(trickLists[i])
+  }
+}
+
+function generateCombo(numTricks) {
+  let trickLists = []
+  let first = Math.floor(Math.random() * allTricks.length)
+  let currTrick = allTricks[first]
+  trickLists.push(currTrick)
+
+  while (trickLists.length < numTricks) {
+    let next = Math.floor(Math.random() * nextTricks[currTrick].length)
+    let nextTrick = nextTricks[currTrick][next]
+    trickLists.push(nextTrick)
+    currTrick = nextTrick
+  }
+  return trickLists
 }
 
 $(document).ready(function(){
-  let trickLists = ["allegra","superman","shoulder_mount","dragonfly"];
+  let combo = generateCombo(4)
+  renderTricks(combo)
 
-  appendTrick(trickLists[0]);
-  for (let i = 1; i < trickLists.length; i++) {
-    appendArrow();
-    appendTrick(trickLists[i]);
-  }
-});
+  $('.regen').on('click',function(){
+    let newCombo = generateCombo(4)
+    renderTricks(newCombo)
+  })
+})
